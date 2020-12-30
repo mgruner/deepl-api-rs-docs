86d8dc275351578a4471a921f9f8248bb3a616fb
Martin Gruner <mg.pub@gmx.net>
2020-12-30 10:03:59 +0100

    Initial commit.

---
+++ source-files.js
@@ -0,0 +1,4 @@
+var N = null;var sourcesIndex = {};
+sourcesIndex["deepl"] = {"name":"","files":["main.rs","parse_arguments.rs"]};
+sourcesIndex["deepl_api"] = {"name":"","files":["lib.rs"]};
+createSourceSidebar();
